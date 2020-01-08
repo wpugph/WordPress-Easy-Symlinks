3a5c3c03e5842c767b8bc384d95d6d0d380a3f47
Carl Alberto <carl.alberto@getpantheon.com>
2020-01-08 13:47:55 +0800

    update everything to version to 1.0.1

--- easy-symlinks.php
+++ easy-symlinks.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: Easy Symlinks
- * Version: 1.0.0
+ * Version: 1.0.1
  * Plugin URI: http://wordpress.org/plugins/easy-symlinks
  * Description: Easy symlinking tool in WP. Best used for non-command line users. This can only track symlinks created within the application and excludes symlinks created from the filesystem and command line. Best used in Pantheon dev environments in SFTP mode.
  * Author: Carl Alberto
